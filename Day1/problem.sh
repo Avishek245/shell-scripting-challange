cca2e32916f9dcef2abbd556ef0a5dd1725b1271
Avishek <avishekghosh245@gmail.com>
2026-03-03 19:29:49 +0000

    Day1 problem solved

---
+++ Day1/problem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# take user name and print
+
+read -p " Enter your name : " User
+
+echo " Hello $User to Shell scripting"
--- Day1/problem1.sh
+++ Day1/problem1.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+#check Even and odd number
+
+read -p " Enter your number " Number
+
+if (( $Number % 2 == 0 ))
+then
+	echo " $Number is even "
+else
+	echo " $Number is odd "
+fi
